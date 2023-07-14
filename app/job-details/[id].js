404738dae796af4b220b76c4fb2aaa55dcddba69
Romane71 <120040498+Romane71@users.noreply.github.com>
2023-07-14 18:47:15 +0100

    job details

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -1,8 +1,58 @@
-import React from "react";
-import { Text } from 'react-native'
+import React, {useCallback, useState} from "react";
+import { Text, View, SafeAreaView, ActivityIndicator, RefreshControl } from 'react-native';
+import { Stack, useRouter, useSearchParams } from "expo-router";
+
+import { Company, JobAbout, JobFooter, JobTabs, ScreenHeaderBtn, Specifics } from '../components';
+import { COLORS, icons, SIZES } from "../constants";
+import useFetch from "../hook/useFetch";
+import { ScrollView } from "react-native-gesture-handler";
+
 
 export const JobDetails = () => {
+const params = useSearchParams();
+const router = useRouter();
+
+const [ data, isLoading, error, refetch] = useFetch('job-details', {
+  job_id: params.id 
+})
+
+const [refreshing, setRefreshing ] = useState(false);
+
+const onRefresh = {}
+
+
     return (
-      <Text>Job Details</Text>
+   <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite  }}
+   >
+    <Stack.Screen 
+    options={{
+   headerStyle: {backgroundColor: COLORS.lightWhite},
+   headerShadowVisible: false,
+   headerBackVisible: false,
+   headerLeft: () => (
+    <ScreenHeaderBtn 
+    iconUrl={icons.left} 
+    dimension="60%"
+    handlePress={() => router.back()}
+    />
+   ),
+      headerRight: () => (
+    <ScreenHeaderBtn 
+    iconUrl={icons.share} 
+    dimension="60%"
+    
+    />
+   ),
+   headerTitle: 'Job Details',
+    }}
+   />
+   <>
+   <ScrollView showsVerticalScrollIndicator={false} 
+   RefreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh}/>}>
+
+   </ScrollView>
+   </>
+
+   </SafeAreaView>
     )
 }
